b137a9be8cd51e06a9ad4c3e9d888a6ecec1af95
Alex-Alessi <alex.alessi246@gmail.com>
2025-01-13 20:12:30 +0100

    Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
 // import { useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import HomePage from "./pages/HomePage";
+import AboutPage from "./pages/AboutPage";
+import DefaultLayout from "./layouts/DefaultLayout";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <>
-      <h1>React Library</h1>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route element={<DefaultLayout />} path="/">
+          <Route index element={<HomePage />}></Route>
+          <Route path="/about" element={<AboutPage />}></Route>
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/AboutPage.jsx
@@ -0,0 +1,7 @@
+export default function AboutPage() {
+  return (
+    <div className="container pt-5">
+      <h1>AboutPage</h1>
+    </div>
+  );
+}
